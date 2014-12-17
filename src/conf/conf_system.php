0cad6d00b9547138d33d8563c97247d057bf33a1
ateliee <ateliee@gmail.com>
2014-12-17 15:45:22 +0900

    console bug fix

--- src/conf/conf_system.php
+++ src/conf/conf_system.php
@@ -7,8 +7,8 @@
 //+++++++++++++++++++++++++++++
 // �V�X�e���f�[�^
 if (defined('SYSTEM_REQUEST') == false) {
-    define('SYSTEM_REQUEST', $_SERVER['REQUEST_URI']);
+    define('SYSTEM_REQUEST', isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : null);
 }
 if (defined('SYSTEM_DOMAIN') == false) {
-    define('SYSTEM_DOMAIN', $_SERVER['SERVER_NAME']);
+    define('SYSTEM_DOMAIN', isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : null);
 }
