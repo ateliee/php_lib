8a21d415737d631373035ecbdad72fa643e32fab
ateliee <ateliee@gmail.com>
2015-06-22 21:25:12 +0900

    change function

--- src/class/class_image.php
+++ src/class/class_image.php
@@ -40,11 +40,18 @@ class class_image
     // コンストラクタ
     function class_image()
     {
+        $this->C_TYPETRUE_DIR = dirname(__FILE__) . "/../exe/ttfont/";
+    }
+
+    /**
+     * @return bool
+     */
+    public function checkGD(){
         // GDライブラリチェック
         if(!function_exists('imagecreatefrompng')){
-            throw new Exception('php-gd uninstall!');
+            return false;
         }
-        $this->C_TYPETRUE_DIR = dirname(__FILE__) . "/../exe/ttfont/";
+        return true;
     }
 
     // 処理の設定
